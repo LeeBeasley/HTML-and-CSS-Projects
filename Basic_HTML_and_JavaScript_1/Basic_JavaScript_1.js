98a1edddc07199b569a0bbd9e8150530d5054b04
LeeBeasley <leshawnbeasley@hotmail.com>
2024-02-23 18:33:19 -0800

    BASIC HTML AND JAVASCRIPT 1 ASSIGNMENT
    assigment

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Here's a pic of ME!!");
+function My_First_Funciton() {
+    var String = "Kiss me, I'm Irish!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = 
+    result;
+}
